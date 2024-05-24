b7da8fe067f3083f4a02334658c4e619db9e4727
Nicolas-Cabrera-Rodriguez <cabrerarodriguezftv@gmail.com>
2024-05-24 15:41:30 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Deshabilitar la caché del navegador
+  win.webContents.session.clearCache();
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ preload.js
@@ -0,0 +1,4 @@
+// preload.js
+window.addEventListener("DOMContentLoaded", () => {
+  // Preload script
+});
